d4e3996aacb67a7997d62f7532861c3c6acf7ddf
kl2376 <146343455+kl2376@users.noreply.github.com>
2023-10-26 19:10:29 +0700

    Add files via upload

---
+++ nok.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    for(int i=1;i<a;i++)
+    {
+        if(a%i==0 && b%i==0)
+        {
+            cout<<"otvet:"<<i;
+            break;
+        }
+    }
+    return 0;
+}
